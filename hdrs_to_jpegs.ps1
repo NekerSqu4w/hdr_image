521e573e6788f1b70531d3ec58e055dd0a325b34
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2024-12-25 12:36:38 +0100

    a

---
+++ hdrs_to_jpegs.ps1
@@ -0,0 +1,20 @@
+$inputDir = "./hdr"
+$outputDir = "./jpg"
+
+# delete old file because ffmpeg need confirmation when rewriting a file
+# there is probably a way to remove confirmation with ffmpeg idk
+Get-ChildItem -Path $outputDir -Filter "*.jpg" | ForEach-Object {
+    $_.Delete()
+}
+
+
+# convert all file
+Get-ChildItem -Path $inputDir -Filter "*.hdr" | ForEach-Object {
+    $inputPath = $_.DirectoryName
+    $inputFile = $inputPath + "\" + $_.Name
+
+    # can be probably optimised
+    $outputFile = $inputPath.Substring(0, $inputPath.LastIndexOf('\')) + $outputDir + "\" + $_.Name.Substring(0, $_.Name.LastIndexOf('.')) + ".jpg"
+     
+    ffmpeg -i $inputFile -vf "tonemap=tonemap=mobius:desat=0:peak=100,eq=gamma=2.1:contrast=1.1,format=rgb24" -q:v 2 $outputFile
+}
