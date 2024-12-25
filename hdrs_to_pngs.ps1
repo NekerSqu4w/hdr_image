49a0d8ecf0a3aa281f0f9ed8df6881923702f7c3
AstalNeker <85320199+NekerSqu4w@users.noreply.github.com>
2024-12-25 11:48:02 +0100

    alot of change because yes

--- hdrs_to_pngs.ps1
+++ hdrs_to_pngs.ps1
@@ -1,6 +1,13 @@
-Get-ChildItem -Path "./" -Filter "*.hdr" | ForEach-Object {
-    $inputFile = $_.FullName
-    $outputFile = "$($inputFile.Substring(0, $inputFile.LastIndexOf('.'))).png"
-    
+$inputDir = "./hdr"
+$outputDir = "\png"
+
+Get-ChildItem -Path $inputDir -Filter "*.hdr" | ForEach-Object {
+    $inputPath = $_.DirectoryName
+    $inputFile = $inputPath + "\" + $_.Name
+
+    # can be probably optimised
+    $outputFile = $inputPath.Substring(0, $inputPath.LastIndexOf('\')) + $outputDir + "\" + $_.Name.Substring(0, $_.Name.LastIndexOf('.')) + ".png"
+
     ffmpeg -i $inputFile -vf tonemap=tonemap=reinhard:desat=0,format=yuv420p -colorspace bt709 -color_primaries bt709 -color_trc bt709 $outputFile
 }
+
